759835753998f611b88203759ceb66912ac44d35
BIPLOB <developerbiplob.bd@gmail.com>
2022-02-23 19:37:26 +0600

    Route | controller | View | csrf

--- laravel-routing/routes/admin.php
+++ laravel-routing/routes/admin.php
@@ -1,9 +1,14 @@
 <<?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\support\Facades\Route;
 
-Route::get('/something', function(){
-    return 'Hello World !';
+Route::get('/something/{name}', function($name){
+    return 'Hello ! I am ' . $name;
 });
 
-?>
+Route::fallback(function(){
+    return view('Errors.404');
+});
+
+Route::get('controller', [TestController::class, 'index']);
