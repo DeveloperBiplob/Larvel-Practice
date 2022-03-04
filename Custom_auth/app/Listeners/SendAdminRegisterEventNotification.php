2dd1a26f1a139ae9c575411a7bb2c58d6c412fbc
BIPLOB <developerbiplob.bd@gmail.com>
2022-03-04 10:42:58 +0600

    Queue

--- Custom_auth/app/Listeners/SendAdminRegisterEventNotification.php
+++ Custom_auth/app/Listeners/SendAdminRegisterEventNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-class SendAdminRegisterEventNotification
+class SendAdminRegisterEventNotification implements ShouldQueue
 {
     /**
      * Create the event listener.
